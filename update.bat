119ab6f9128478d35baadc9c181f255db2dc3698
WhyFear <893843891@qq.com>
2020-03-24 18:17:17 +0800

    new config  2020-03-24-08:17

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "new config  %date:~0,4%%date:~5,2%%date:~8,2%0%time:~1,1%%time:~3,2%%time:~6,2%"
+git commit -m "new config  %date:~0,4%-%date:~5,2%-%date:~8,2%-0%time:~1,1%:%time:~3,2%"
 git push
