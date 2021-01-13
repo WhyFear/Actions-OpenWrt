6d3b38efa2c08719fde8c07c6fa982e56df88b95
ZengLH <893843891@qq.com>
2021-01-13 11:25:29 +0800

    new config  2021-01-13-11:25

--- diy_for_x86.sh
+++ diy_for_x86.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.199.2/g' package/base-files/files/bin/config_generate
+sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
