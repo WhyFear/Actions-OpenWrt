7871a8700859c2a255b0821c560aa6927cb6933d
WhyFear <893843891@qq.com>
2021-04-26 15:08:25 +0800

    new config  2021-04-26-15:08

---
+++ diy_for_L1Pro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Modify default IP L1Pro 默认123.2 .1是预留给ap的
+sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把Pakro修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Pakro'' package/lean/default-settings/files/zzz-default-settings
+
+# 版本号里显示一个自己的名字（Pakro build for L1Pro$(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
+sed -i "s/OpenWrt /Pakro build for L1Pro$(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
--- diy_for_x86.sh
+++ diy_for_x86.sh
@@ -12,4 +12,4 @@ sed -i 's/192.168.1.1/192.168.199.2/g' package/base-files/files/bin/config_gener
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Pakro'' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（Pakro build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/OpenWrt /Pakro build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /Pakro build for x86$(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
