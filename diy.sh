e77d2288c3b0db980e782d61f47aa8da2643546f
WhyFear <893843891@qq.com>
2021-04-29 20:09:01 +0800

    test

--- diy.sh
+++ diy.sh
@@ -13,3 +13,5 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='Pakro'' package
 
 # 版本号里显示一个自己的名字（Pakro build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /Pakro build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+echo "diy.sh done!"
